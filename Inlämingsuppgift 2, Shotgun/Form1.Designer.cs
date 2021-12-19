d2b7604ff7aeb67e7a37f766048573efaffec8c5
Aleksandar Trajkovic <Aleksandar.trajkovic01@gmail.com>
2021-12-19 18:23:55 +0100

    Shotgun-Game complete

---
+++ Inlämingsuppgift 2, Shotgun/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace Inlämingsuppgift_2__Shotgun
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonShoot = new System.Windows.Forms.Button();
+            this.buttonReload = new System.Windows.Forms.Button();
+            this.buttonBlock = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.buttonAgain = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPlayer = new System.Windows.Forms.TextBox();
+            this.textBoxCPU = new System.Windows.Forms.TextBox();
+            this.labelShells = new System.Windows.Forms.Label();
+            this.labelShellsCPU = new System.Windows.Forms.Label();
+            this.buttonShotgun = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonShoot
+            // 
+            this.buttonShoot.Location = new System.Drawing.Point(12, 78);
+            this.buttonShoot.Name = "buttonShoot";
+            this.buttonShoot.Size = new System.Drawing.Size(102, 34);
+            this.buttonShoot.TabIndex = 0;
+            this.buttonShoot.Text = "Shoot";
+            this.buttonShoot.UseVisualStyleBackColor = true;
+            this.buttonShoot.Click += new System.EventHandler(this.buttonShoot_Click);
+            // 
+            // buttonReload
+            // 
+            this.buttonReload.Location = new System.Drawing.Point(12, 140);
+            this.buttonReload.Name = "buttonReload";
+            this.buttonReload.Size = new System.Drawing.Size(102, 33);
+            this.buttonReload.TabIndex = 1;
+            this.buttonReload.Text = "Reload";
+            this.buttonReload.UseVisualStyleBackColor = true;
+            this.buttonReload.Click += new System.EventHandler(this.buttonReload_Click);
+            // 
+            // buttonBlock
+            // 
+            this.buttonBlock.Location = new System.Drawing.Point(12, 206);
+            this.buttonBlock.Name = "buttonBlock";
+            this.buttonBlock.Size = new System.Drawing.Size(102, 32);
+            this.buttonBlock.TabIndex = 2;
+            this.buttonBlock.Text = "Block";
+            this.buttonBlock.UseVisualStyleBackColor = true;
+            this.buttonBlock.Click += new System.EventHandler(this.buttonBlock_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(213, 77);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(207, 161);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Snap ITC", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(465, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 35);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "VS";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Inlämingsuppgift_2__Shotgun.Properties.Resources.cpu_processor_vector_icon_isolated_white_background_149262951;
+            this.pictureBox2.Location = new System.Drawing.Point(560, 77);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(228, 161);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // buttonAgain
+            // 
+            this.buttonAgain.Location = new System.Drawing.Point(12, 385);
+            this.buttonAgain.Name = "buttonAgain";
+            this.buttonAgain.Size = new System.Drawing.Size(102, 33);
+            this.buttonAgain.TabIndex = 6;
+            this.buttonAgain.Text = "Play again";
+            this.buttonAgain.UseVisualStyleBackColor = true;
+            this.buttonAgain.Click += new System.EventHandler(this.buttonAgain_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(560, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Shells:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(213, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Shells:";
+            // 
+            // textBoxPlayer
+            // 
+            this.textBoxPlayer.Location = new System.Drawing.Point(213, 244);
+            this.textBoxPlayer.Name = "textBoxPlayer";
+            this.textBoxPlayer.Size = new System.Drawing.Size(207, 23);
+            this.textBoxPlayer.TabIndex = 11;
+            // 
+            // textBoxCPU
+            // 
+            this.textBoxCPU.Location = new System.Drawing.Point(560, 244);
+            this.textBoxCPU.Name = "textBoxCPU";
+            this.textBoxCPU.Size = new System.Drawing.Size(228, 23);
+            this.textBoxCPU.TabIndex = 12;
+            // 
+            // labelShells
+            // 
+            this.labelShells.AutoSize = true;
+            this.labelShells.Location = new System.Drawing.Point(259, 49);
+            this.labelShells.Name = "labelShells";
+            this.labelShells.Size = new System.Drawing.Size(13, 15);
+            this.labelShells.TabIndex = 13;
+            this.labelShells.Text = "0";
+            // 
+            // labelShellsCPU
+            // 
+            this.labelShellsCPU.AutoSize = true;
+            this.labelShellsCPU.Location = new System.Drawing.Point(607, 48);
+            this.labelShellsCPU.Name = "labelShellsCPU";
+            this.labelShellsCPU.Size = new System.Drawing.Size(13, 15);
+            this.labelShellsCPU.TabIndex = 14;
+            this.labelShellsCPU.Text = "0";
+            // 
+            // buttonShotgun
+            // 
+            this.buttonShotgun.Location = new System.Drawing.Point(12, 270);
+            this.buttonShotgun.Name = "buttonShotgun";
+            this.buttonShotgun.Size = new System.Drawing.Size(102, 32);
+            this.buttonShotgun.TabIndex = 15;
+            this.buttonShotgun.Text = "SHOTGUN";
+            this.buttonShotgun.UseVisualStyleBackColor = true;
+            this.buttonShotgun.Click += new System.EventHandler(this.buttonShotgun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonShotgun);
+            this.Controls.Add(this.labelShellsCPU);
+            this.Controls.Add(this.labelShells);
+            this.Controls.Add(this.textBoxCPU);
+            this.Controls.Add(this.textBoxPlayer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonAgain);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonBlock);
+            this.Controls.Add(this.buttonReload);
+            this.Controls.Add(this.buttonShoot);
+            this.Name = "Form1";
+            this.Text = "Shotgun game";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Button buttonShoot;
+        public System.Windows.Forms.Button buttonReload;
+        public System.Windows.Forms.Button buttonBlock;
+        public System.Windows.Forms.Button buttonAgain;
+        public System.Windows.Forms.TextBox textBoxPlayer;
+        public System.Windows.Forms.TextBox textBoxCPU;
+        public System.Windows.Forms.Label labelShells;
+        public System.Windows.Forms.Label labelShellsCPU;
+        public System.Windows.Forms.Button buttonShotgun;
+    }
+}
+
---
+++ Inlämingsuppgift 2, Shotgun/Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inlämingsuppgift_2__Shotgun
+{
+    public partial class Form1 : Form
+    {
+        
+        CPU Bot = new CPU();
+        Player Me = new Player();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void ClearTextBoxes()
+        {
+            textBoxCPU.Clear();
+            textBoxPlayer.Clear();
+        }
+
+        public class CPU
+        {
+            public int CPUShells { get; set; }
+
+            public string[] Choice = new string[] { "Shoot", "Reload", "Block" };
+
+            public int RandomNumber = 0;
+
+            public Random CPUSelection = new Random();
+            public string CPUChoice { get; set; }
+
+            public void RandomChoice()
+            {
+                RandomNumber = CPUSelection.Next(0, Choice.Length);
+
+                CPUChoice = Choice[RandomNumber];
+            }
+        }
+
+        public class Player
+        {
+            public int PlayerShells { get; set; }
+        }
+       
+        public void buttonShoot_Click(object sender, EventArgs e)
+        {          
+            Bot.RandomChoice();
+            
+            if (Bot.CPUShells == 3)
+            {
+                ClearTextBoxes();
+                Me.PlayerShells--;
+                Bot.CPUShells = 0;
+                labelShellsCPU.Text = Bot.CPUShells.ToString();
+                labelShells.Text = Me.PlayerShells.ToString();
+                textBoxPlayer.Text = "Player shoots";
+                textBoxCPU.Text = "CPU uses Shotgun";
+                MessageBox.Show("CPU won the game by Using Shotgun!");
+                Bot.RandomNumber = 4;
+            }
+                      
+            if (Bot.RandomNumber == 2)
+            {
+               
+                if (Me.PlayerShells == 0)
+                {
+                    ClearTextBoxes();
+                    textBoxPlayer.Text = "Player tried to shoot but have no shells";
+                    textBoxCPU.Text = "CPU blocks";
+                }
+                else if (Bot.RandomNumber == 2)
+                {
+                    ClearTextBoxes();
+                    Me.PlayerShells--;
+                    labelShells.Text = Me.PlayerShells.ToString();
+                    textBoxPlayer.Text = "Player shoots";
+                    textBoxCPU.Text = "CPU blocks";
+                }               
+                
+            }
+            else if (Bot.RandomNumber == 0)
+            {
+                if (Bot.CPUShells == 1 || Bot.CPUShells == 2 && Me.PlayerShells == 1 || Me.PlayerShells == 2)
+                {
+                    ClearTextBoxes();
+                    Me.PlayerShells--;
+                    Bot.CPUShells--;
+                    labelShellsCPU.Text = Bot.CPUShells.ToString();
+                    labelShells.Text = Me.PlayerShells.ToString();
+                    textBoxPlayer.Text = "Player shoots";
+                    textBoxCPU.Text = "CPU shoots";
+                }
+                else if (Me.PlayerShells == 0 && Bot.CPUShells == 1 || Bot.CPUShells == 2)
+                {
+                    ClearTextBoxes();
+                    Bot.CPUShells--;
+                    labelShellsCPU.Text = Bot.CPUShells.ToString();
+                    textBoxCPU.Text = "CPU shoots";
+                    textBoxPlayer.Text = "Player tried to shoot but had no shells";
+                    MessageBox.Show("CPU has won the game by shooting Player");
+                }
+                else if (Bot.CPUShells == 0 && Me.PlayerShells == 1 || Me.PlayerShells == 2)
+                {
+                    Bot.RandomNumber = 1;
+                    ClearTextBoxes();
+                    Me.PlayerShells--;
+                    Bot.CPUShells++;
+                    labelShells.Text = Me.PlayerShells.ToString();
+                    textBoxPlayer.Text = "Player shoots";
+                    textBoxCPU.Text = "CPU reloads";
+                    MessageBox.Show("Player has won the game by shooting CPU");
+                }
+                else if (Me.PlayerShells == 0 && Bot.CPUShells == 0)
+                {
+                    Bot.RandomNumber = 1;
+                    ClearTextBoxes();
+                    Bot.CPUShells++;
+                    labelShellsCPU.Text = Bot.CPUShells.ToString();
+                    textBoxPlayer.Text = "Player tried to shoot but had no shells";
+                    textBoxCPU.Text = "CPU reloads";
+                }                                                                     
+                                
+            }
+            else if (Bot.RandomNumber == 1)
+            {
+                if (Me.PlayerShells == 0)
+                {
+                    ClearTextBoxes();
+                    Bot.CPUShells++;
+                    labelShellsCPU.Text = Bot.CPUShells.ToString();
+                    textBoxPlayer.Text = "Player tried to shoot but have no shells";
+                    textBoxCPU.Text = "CPU reloads";
+                }
+                else if (Me.PlayerShells == 1 || Me.PlayerShells == 2)
+                {
+                    ClearTextBoxes();
+                    Me.PlayerShells--;
+                    Bot.CPUShells++;
+                    labelShells.Text = Me.PlayerShells.ToString();
+                    labelShellsCPU.Text = Bot.CPUShells.ToString();
+                    textBoxPlayer.Text = "Player shoots";
+                    textBoxCPU.Text = "CPU Reloads";
+                    MessageBox.Show("Player has won the game by shooting CPU");
+                }               
+            }           
+        }
+        
+        public void buttonReload_Click(object sender, EventArgs e)
+        {           
+
+            Bot.RandomChoice();
+
+            if (Bot.CPUShells == 3)
+            {
+                ClearTextBoxes();
+                Me.PlayerShells++;
+                Bot.CPUShells = 0;
+                labelShellsCPU.Text = Bot.CPUShells.ToString();
+                labelShells.Text = Me.PlayerShells.ToString();
+                textBoxPlayer.Text = "Player reloads";
+                textBoxCPU.Text = "CPU uses Shotgun";
+                MessageBox.Show("CPU won the game by Using Shotgun!");
+                Bot.RandomNumber = 4;
+            }
+                      
+            if (Bot.RandomNumber == 1)
+            {
+                ClearTextBoxes();
+                Me.PlayerShells++;
+                Bot.CPUShells++;
+                labelShellsCPU.Text = Bot.CPUShells.ToString();
+                labelShells.Text = Me.PlayerShells.ToString();
+                textBoxPlayer.Text = "Player reloads";
+                textBoxCPU.Text = "CPU Reloads";
+            }
+            else if (Bot.RandomNumber == 2)
+            {
+                ClearTextBoxes();
+                Me.PlayerShells++;
+                labelShells.Text = Me.PlayerShells.ToString();
+                textBoxPlayer.Text = "Player reloads";
+                textBoxCPU.Text = "CPU blocks";
+            }
+            else if (Bot.RandomNumber == 0)
+            {
+                if (Bot.CPUShells == 0)
+                {
+                    Bot.RandomNumber = 1;
+                    ClearTextBoxes();
+                    Me.PlayerShells++;
+                    Bot.CPUShells++;
+                    labelShellsCPU.Text = Bot.CPUShells.ToString();
+                    labelShells.Text = Me.PlayerShells.ToString();
+                    textBoxPlayer.Text = "Player reloads";
+                    textBoxCPU.Text = "CPU reloads";
+                }
+                else if (Bot.CPUShells == 1 || Bot.CPUShells == 2)
+                {
+                    ClearTextBoxes();
+                    Bot.CPUShells--;
+                    Me.PlayerShells++;
+                    labelShells.Text = Me.PlayerShells.ToString();
+                    labelShellsCPU.Text = Bot.CPUShells.ToString();
+                    textBoxPlayer.Text = "Player reloads";
+                    textBoxCPU.Text = "CPU shoots";
+                    MessageBox.Show("CPU has won the game by shooting Player");
+                }                
+            }
+                       
+        }
+       
+
+        public void buttonBlock_Click(object sender, EventArgs e)
+        {
+           
+            Bot.RandomChoice();
+
+            if (Bot.CPUShells == 3)
+            {
+                ClearTextBoxes();
+                Bot.CPUShells = 0;
+                labelShellsCPU.Text = Bot.CPUShells.ToString();
+                textBoxPlayer.Text = "Player blocks";
+                textBoxCPU.Text = "CPU uses Shotgun";
+                MessageBox.Show("CPU won the game by Using Shotgun!");
+                Bot.RandomNumber = 4;
+            }
+                     
+
+            if (Bot.RandomNumber == 0)
+            {
+                if (Bot.CPUShells == 0)
+                {
+                    Bot.RandomNumber = 1;
+                    ClearTextBoxes();
+                    Bot.CPUShells++;
+                    labelShellsCPU.Text = Bot.CPUShells.ToString();
+                    textBoxPlayer.Text = "Player blocks";
+                    textBoxCPU.Text = "CPU reloads";
+                }
+                else if (Bot.CPUShells == 1 || Bot.CPUShells == 2)
+                {
+                    ClearTextBoxes();
+                    Bot.CPUShells--;
+                    labelShellsCPU.Text = Bot.CPUShells.ToString();
+                    textBoxPlayer.Text = "Player blocks";
+                    textBoxCPU.Text = "CPU shoots";
+                }                                               
+                                                
+            }
+            else if (Bot.RandomNumber == 1)
+            {
+                ClearTextBoxes();
+                Bot.CPUShells++;
+                labelShellsCPU.Text = Bot.CPUShells.ToString();
+                textBoxPlayer.Text = "Player blocks";
+                textBoxCPU.Text = "CPU reloads";
+            }
+            else if (Bot.RandomNumber == 2)
+            {
+               ClearTextBoxes();
+                textBoxPlayer.Text = "Player blocks";
+                textBoxCPU.Text = "CPU blocks";
+            }
+           
+
+        }
+
+        public void buttonShotgun_Click(object sender, EventArgs e)
+        {
+
+            if (Me.PlayerShells == 3)
+            {
+                if (Me.PlayerShells == 3 && Bot.CPUShells == 3)
+                {                    
+                    Bot.RandomNumber = 4;
+                    ClearTextBoxes();
+                    Bot.CPUShells = 0;
+                    Me.PlayerShells = 0;
+                    labelShells.Text = Me.PlayerShells.ToString();
+                    labelShellsCPU.Text = Bot.CPUShells.ToString();
+                    textBoxCPU.Text = "CPU used shotgun";
+                    textBoxPlayer.Text = "Player used shotgun";
+                    MessageBox.Show("It's a draw!");
+
+                }
+                else if (Me.PlayerShells == 3)
+                {
+                    ClearTextBoxes();
+                    Me.PlayerShells = 0;
+                    labelShells.Text = Me.PlayerShells.ToString();
+                    textBoxPlayer.Text = "Player used Shoutgun";
+                    textBoxCPU.Text = "CPU did not stand a chance against the Shotgun";
+                    MessageBox.Show("Player has won the game by using Shotgun");
+                }                
+                
+            }
+            else if (Me.PlayerShells == 0 || Me.PlayerShells == 1 || Me.PlayerShells == 2)
+            {
+                ClearTextBoxes();
+                textBoxPlayer.Text = "Player does not have 3 shells";
+                textBoxCPU.Text = "CPU is waiting for Player to make a move";
+            }
+            
+        }
+
+        public void buttonAgain_Click(object sender, EventArgs e)
+        {
+            ClearTextBoxes();
+
+            Bot.CPUShells = 0;
+            Me.PlayerShells = 0;
+            labelShells.Text = Me.PlayerShells.ToString();
+            labelShellsCPU.Text = Bot.CPUShells.ToString();
+        }
+        
+    }
+}
